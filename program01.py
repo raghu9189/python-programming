ba846e8fdf35dc3302b2d23d6086238a7fb866cb
raghu9189 <raghu.ballu@gmail.com>
2026-06-14 10:43:09 +0530

    advanced data types

---
+++ program01.py
@@ -0,0 +1,19 @@
+d = dict()
+l = list()
+s = set()
+t = (2,3,4)
+fs =frozenset((2,3,4,5))
+
+d['name'] = "Raghu"
+l.append('Raghu')
+s.add(1)
+s.add(2)
+
+
+print(d)
+print(s)
+print(t)
+
+
+for i in range(0, 5, 1):
+    print(i)
