f8b5cf1ea951e51d4693ce400349f4714dab5771
raghu9189 <raghu.ballu@gmail.com>
2026-09-26 22:05:03 +0530

    add lambda func

--- lambda-func.py
+++ lambda-func.py
@@ -1,5 +1,5 @@
 # what is lambda function ?
 # A function is without name is called lambda function
-add = lambda a, b: a + b
+add = lambda a, b: a + b # Also known for First class Function
 
 print(add(1,2))
